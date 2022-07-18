ddf398d8ec64134bdc24aff74ad5a2ccf7eaceae
Shdevsir <shdevsir@gmail.com>
2022-07-19 00:03:51 +0300

    Add route with secret phrase

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,7 +11,15 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
+class SecretPhrase(Resource):
+    def get(self):
+        question = os.environ.get("QUESTION")
+        answer = os.environ.get("ANSWER")
+        return {question: answer}
+
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(SecretPhrase, '/secret')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,8 +1,26 @@
-from app import app # Flask instance of the API
+from app import app
+from mock import patch
+from pytest import fixture
+import os
 import json
 
-def test_index_route():
+
+@fixture
+def env_var():
+    with patch.dict(os.environ, {"QUESTION": "How are you?",
+                                 "ANSWER": "Fine, thanks"}):
+        yield
+
+
+def test_hello_route():
     response = app.test_client().get('/')
+    res = json.loads(response.data.decode('utf-8'))
+    assert response.status_code == 200
+    assert res == {"hello": "world"}
+
+
+def test_secret_route(env_var):
+    response = app.test_client().get('/secret')
     res = json.loads(response.data.decode('utf-8'))
     assert response.status_code == 200
     assert res == {"hello": "world"}
