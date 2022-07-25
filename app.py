68292e9b5f950a265afb9c75ff409c4e2db380cc
Shdevsir <shdevsir@gmail.com>
2022-07-25 22:49:25 +0300

    Os env param

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(SecretPhrase, '/secret')
 
 if __name__ == '__main__':
-    app.run(host='localhost', port='5000', debug=True)
+    app.run(debug=True)
