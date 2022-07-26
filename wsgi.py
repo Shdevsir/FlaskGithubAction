70ae8bf0dc007f91942896c9391e13dfea0027e3
Shdevsir <shdevsir@gmail.com>
2022-07-26 11:36:20 +0300

    Os env param

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 from app import app
+import os
 
+host = os.environ.get("FLASK_HOST")
+port = os.environ.get("FLASK_PORT")
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host=host, port=port, debug=False)
